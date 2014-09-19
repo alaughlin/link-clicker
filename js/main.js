07b6c3891dbdfeb41dee6a1efb5f549219cb71f0
alaughlin <adamlaughlin@outlook.com>
2014-09-19 14:43:52 -0400

    First commit.

---
+++ js/main.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+  $.get("../sentences.txt", function (data) {
+    var lines = data.split("\n");
+    _.each(lines, function (line) {
+      $('#sentences').append('<li><a href="#" class="sentence-link">' + line + '</a></li>');
+    });
+  });
+
+  $('#sentences').on('click', '.sentence-link', function (event) {
+    event.preventDefault();
+    var sentence = $(event.currentTarget).html()
+    $.ajax({
+      url: "/",
+      success: function () {
+        $('#sentence-display').html(sentence);
+      }
+    });
+  });
+});
